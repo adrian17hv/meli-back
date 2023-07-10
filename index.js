20a607eb36042ec43d451b5a83afeb4041b0822c
adrian <adrianhugov@gmail.com>
2023-07-09 22:06:34 -0300

    add all route

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const itemsRouter = require("./api/items");
 const app = express();
 app.use(cors());
 app.all("*", authorMiddleware);
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 💖');
+})
 app.use("/items", itemsRouter);
 
 app.listen(8000, () => console.log("Server is up!"));
