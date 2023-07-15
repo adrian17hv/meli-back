a9c9d023b1db88bab4b6d4c025a61df14a57eca2
adrian <adrianhugov@gmail.com>
2023-07-15 12:04:33 -0300

    cors

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ const itemsRouter = require("./api/items");
 
 const app = express();
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Reemplaza con tu origen permitido
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.all("*", authorMiddleware);
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 💖');
-})
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 💖");
+});
 app.use("/items", itemsRouter);
 
 app.listen(8000, () => console.log("Server is up!"));
 
-
-app.get("/items")
-app.get("/items/:id")
+app.get("/items");
+app.get("/items/:id");
