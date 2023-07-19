3e16b8215f76bc36822cf57a596249d0eb420c38
adrian <adrianhugov@gmail.com>
2023-07-19 20:47:41 -0300

    cors

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Reemplaza con tu origen permitido
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://meli-front-pearl.vercel.app"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
