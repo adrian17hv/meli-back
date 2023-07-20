f98d51ac5f2c0562125d0a13d53f793fa546e1c9
adrian <adrianhugov@gmail.com>
2023-07-20 19:35:18 -0300

    vercel.json added

--- index.js
+++ index.js
@@ -6,22 +6,10 @@ const itemsRouter = require("./api/items");
 const app = express();
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://meli-front-pearl.vercel.app"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 app.all("*", authorMiddleware);
 app.get("/", (req, res) => {
   res.send("Hey this is my API running 💖");
 });
 app.use("/items", itemsRouter);
-
-app.get("/items/:id");
 
 app.listen(8000, () => console.log("Server is up!"));
