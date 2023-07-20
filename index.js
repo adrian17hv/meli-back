135ae08120bad1cc2702b5acc8352a31dd88764e
adrian <adrianhugov@gmail.com>
2023-07-19 21:10:32 -0300

    error

--- index.js
+++ index.js
@@ -6,16 +6,6 @@ const itemsRouter = require("./api/items");
 const app = express();
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://meli-front-pearl.vercel.app"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 app.all("*", authorMiddleware);
 app.get("/", (req, res) => {
   res.send("Hey this is my API running 💖");
