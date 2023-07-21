de99abacc08d6e9ed468b0231f47df4c19730ee4
adrian <adrianhugov@gmail.com>
2023-07-21 11:18:50 -0300

    package.json modified, added folders for netlify deploy, deleted vercel folders

---
+++ functions/api.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const { authorMiddleware } = require("./authorMiddleware");
+const itemsRouter = require("./api/items");
+
+const app = express();
+app.use(cors());
+
+app.all("*", authorMiddleware);
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 💖");
+});
+app.use("/items", itemsRouter);
+
+app.listen(8000, () => console.log("Server is up!"));
